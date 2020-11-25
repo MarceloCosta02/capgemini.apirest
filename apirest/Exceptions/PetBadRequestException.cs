dbdaac78a31d9c05986b0966acc984410d434ea3
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-24 21:48:37 -0300

    Modificando as exceptions personalizadas

---
+++ apirest/Exceptions/PetBadRequestException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apirest.Exceptions
+{
+    public class PetBadRequestException : ApplicationException
+    {
+        /// <summary> 
+        /// Excessão personalizada da classe Pets para Bad Request 400
+        /// </summary>
+        public PetBadRequestException(string message) : base(message) { }
+    }
+}
---
+++ apirest/Exceptions/PetNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apirest.Exceptions
+{
+    public class PetNotFoundException : ApplicationException
+    {
+        /// <summary> 
+        /// Excessão personalizada da classe Pets para Not Found 404
+        /// </summary>
+        public PetNotFoundException(string message) : base(message) { }
+    }
+}
